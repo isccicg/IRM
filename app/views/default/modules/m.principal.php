0ad3236383cdd71ec26483572e9441aee5316a55
USERPC <USERPC@Lenovo-PC>
2018-10-10 20:29:22 -0500

    Se sube el template general para el proyecto IRM Christian Castaneda sigue teniendo el control para modificacion del mismo

---
+++ app/views/default/modules/m.principal.php
@@ -0,0 +1,284 @@
+<div class="preloader">
+    <div class="preloder-wrap">
+      <div class="preloder-inner"> 
+        <div class="ball"></div> 
+        <div class="ball"></div> 
+        <div class="ball"></div> 
+        <div class="ball"></div> 
+        <div class="ball"></div> 
+        <div class="ball"></div> 
+        <div class="ball"></div>
+      </div>
+    </div>
+</div><!--/.preloader-->
+
+<header id="navigation"> 
+  <div class="navbar navbar-inverse navbar-fixed-top" role="banner"> 
+    <div class="container"> 
+        <div class="navbar-header"> 
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"> 
+            <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> 
+          </button> 
+          <a class="navbar-brand" href="index.html"><h1>Soluciones Tecnologicas de Servisios EDS<img src="" alt=""></h1></a> 
+        </div> 
+        <div class="collapse navbar-collapse"> 
+          <ul class="nav navbar-nav navbar-right"> 
+            <li class="scroll active"><a href="#navigation">Inicio</a></li> 
+            <li class="scroll"><a href="#about-us">Acerca de</a></li> 
+            <li class="scroll"><a href="#services">Servicios</a></li> 
+            <li class="scroll"><a href="#clients">Clientes</a></li> 
+            <li class="scroll"><a href="#contact">Contacto</a></li> 
+          </ul> 
+        </div> 
+    </div> 
+  </div><!--/navbar--> 
+</header> <!--/#navigation--> 
+
+<section id="home">
+  <div class="home-pattern"></div>
+    <div id="main-carousel" class="carousel slide" data-ride="carousel"> 
+      <!-- Start Slider-->
+      <div id="wowslider-container1">
+        <div class="ws_images">
+          <ul>
+            <li>
+              <img src="app/views/default/efectos/images/slider/imagen1.jpg" alt="imagen1" title="" id="wows1_0"/>
+              <h2 class="heading animated bounceInDown">Desarrollo de Aplicaciones</h2>
+              <h3><p class="animated bounceInUp">Calidad, eficiencia y funcionalidad.</p></h3>
+            </li>
+            <li>
+              <img src="app/views/default/efectos/images/slider/imagen2.jpg" alt="imagen2" title="" id="wows1_1"/>
+              <h3><p class="animated bounceInUp">Nuestro equipo de trabajo crea aplicaciones web y de escritorio, de acuerdo a tus necesidades.</p></h3>
+            </li>
+            <li>
+              <img src="app/views/default/efectos/images/slider/imagen3.jpg" alt="imagen3" title="" id="wows1_2"/>
+              <h2 class="heading animated bounceInDown">Instalación de Redes</h2>
+              <h3><p class="animated bounceInUp">Comunicación a tu alcance.</p></h3>
+              </li>
+            <li>
+              <img src="app/views/default/efectos/images/slider/imagen4.jpg" alt="imagen4" title="" id="wows1_3"/>
+              <!-- <h2 class="heading animated bounceInDown">Desarrollo de Aplicaciones</h2> -->
+              <h3><p class="animated bounceInUp">Nuestro equipo de trabajo diseña e instala redes de computadoras, de acuerdo a tus necesidades</p></h3>
+            </li>
+            <li>
+              <img src="app/views/default/efectos/images/slider/imagen5.jpg" alt="imagen5" title="" id="wows1_4"/>
+              <h2 class="heading animated bounceInDown">Mantenimiento y Soporte</h2>
+              <h3><p class="animated bounceInUp">Garantizamos el funcionamiento óptimo de los sistemas y equipos.</p></h3>
+            </li>
+            <li>
+              <img src="app/views/default/efectos/images/slider/imagen6.jpg" alt="imagen6" title="" id="wows1_5"/>
+              <!-- <h2 class="heading animated bounceInDown">Desarrollo de Aplicaciones</h2> -->
+              <h3><p class="animated bounceInUp">Mantenimiento Preventivo y Correctivo.</p></h3>
+            </li>
+          </ul>
+        </div>
+        <div class="ws_bullets">
+          <div>
+            <a href="#" title="imagen1"><span><img src="app/views/default/efectos/images/tooltips/imagen1.jpg" alt="imagen1"/>1</span></a>
+            <a href="#" title="imagen2"><span><img src="app/views/default/efectos/images/tooltips/imagen2.jpg" alt="imagen2"/>2</span></a>
+            <a href="#" title="imagen3"><span><img src="app/views/default/efectos/images/tooltips/imagen3.jpg" alt="imagen3"/>3</span></a>
+            <a href="#" title="imagen4"><span><img src="app/views/default/efectos/images/tooltips/imagen4.jpg" alt="imagen4"/>4</span></a>
+            <a href="#" title="imagen5"><span><img src="app/views/default/efectos/images/tooltips/imagen5.jpg" alt="imagen5"/>5</span></a>
+            <a href="#" title="imagen6"><span><img src="app/views/default/efectos/images/tooltips/imagen6.jpg" alt="imagen6"/>6</span></a>
+          </div>
+        </div>
+      </div>
+      <!-- End Slider -->
+    </div> 
+</section><!--/#home-->
+
+<section id="about-us">
+  <div class="container">
+    <div class="text-center">
+      <div class="col-sm-8 col-sm-offset-2">
+        <h2 class="title-one animate-in" data-anim-type="fade-in-up" data-anim-delay="0">¿Por qué con nosotros?</h2>
+        <p class="animate-in" data-anim-type="fade-in-up" data-anim-delay="100">Somos una empresa, que cuenta con un equipo de trabajo altamente calificado y con el suficiente conocimiento para brindar soluciones tecnológicas.</p>
+      </div>
+    </div>
+    <div class="about-us">
+      <div class="row">
+        <div class="col-sm-6 animate-in super-slow-mo" data-anim-type="fade-in-up" data-anim-delay="0">
+          <h3>¿QUIÉNES SOMOS?</h3>
+          <ul class="nav nav-tabs">
+            <li class="active"><a href="#about" data-toggle="tab"><i class="fa fa-chain-broken"></i> MISIÓN</a></li>
+            <li><a href="#mission" data-toggle="tab"><i class="fa fa-th-large"></i> VISIÓN</a></li>
+            <li><a href="#community" data-toggle="tab"><i class="fa fa-users"></i> OBJETIVO</a></li>
+          </ul>
+          <div class="tab-content">
+            <div class="tab-pane fade in active" id="about">
+              <div class="media animated fadeIn">
+                <img class="pull-left media-object" src="app/views/default/efectos/images/about/mision.png" alt="about us"> 
+                <div class="media-body">
+                 <p> Nuestra misión es brindar servicios de calidad y desarrollar soluciones tecnológicas que ayuden a nuestros clientes a gestionar y optimizar los procesos de manera rápida y eficiente.</p>
+                </div>
+              </div>
+            </div>
+            <div class="tab-pane fade" id="mission">
+              <div class="media animated fadeIn">
+                <img class="pull-left media-object" src="app/views/default/efectos/images/about/vision.png" alt="Mission"> 
+                <div class="media-body">
+                 <p> Para este 2017 ser una empresa líder en el ramo, la cual satisfaga las necesidades de los clientes y las empresas que requieran nuestros servicios, y a su vez dar la confianza de que al elegirnos ellos están haciendo una buena elección.</p>              
+               </div>
+              </div>
+            </div>
+            <div class="tab-pane fade" id="community">
+              <div class="media animated fadeIn">
+                <img class="pull-left media-object" src="app/views/default/efectos/images/about/objetivo.png" alt="Community"> 
+                <div class="media-body">
+                  <p> Dar soluciones tecnológicas que ayuden a los clientes a generar un trabajo rápido y eficiente en oficinas o negocios. </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-sm-6 animate-in super-slow-mo" data-anim-type="fade-in-up" data-anim-delay="0">
+          <h3>NUESTRAS HABILIDADES</h3>
+          <div class="skill-bar">
+            <div class="skillbar clearfix " data-percent="90%">
+              <div class="skillbar-title">
+                <span>HTML5 &amp; CSS3</span>
+              </div>
+              <div class="skillbar-bar"></div>
+              <div class="skill-bar-percent">90%</div>
+            </div> <!-- End Skill Bar -->
+            <div class="skillbar clearfix" data-percent="95%">
+              <div class="skillbar-title"><span>UI Design</span></div>
+              <div class="skillbar-bar"></div>
+              <div class="skill-bar-percent">95%</div>
+            </div> <!-- End Skill Bar -->
+            <div class="skillbar clearfix " data-percent="90%">
+              <div class="skillbar-title"><span>jQuery</span></div>
+              <div class="skillbar-bar"></div>
+              <div class="skill-bar-percent">90%</div>
+            </div> <!-- End Skill Bar -->
+            <div class="skillbar clearfix " data-percent="90%">
+              <div class="skillbar-title"><span>PHP</span></div>
+              <div class="skillbar-bar"></div>
+              <div class="skill-bar-percent">90%</div>
+            </div> <!-- End Skill Bar -->
+            <div class="skillbar clearfix " data-percent="85%">
+              <div class="skillbar-title"><span>HTML5</span></div>
+              <div class="skillbar-bar"></div>
+              <div class="skill-bar-percent">85%</div>
+            </div> <!-- End Skill Bar --></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section><!--/#about-us-->
+
+  <section id="services" class="parallax-section pattern_home">
+    <div class="container">
+      <div class="row text-center">
+        <div class="col-sm-8 col-sm-offset-2 animate-in" data-anim-type="fade-in-up" data-anim-delay="0">
+          <h2 class="title-one">Servicios</h2>
+          <p class="single-service">“Lo que haces es infinitamente más importante que cómo lo haces. La eficiencia sigue importando, pero es inútil si no la aplicas a lo correcto.” – Timothy Ferriss.</p>
+          <p class="single-service">EDS te ofrece los siguientes servicios, desarrollo de software, creación de redes y mantenimiento a equipo de cómputo para mantener tu negocio o empresa actualizada y a la vanguardia.</p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-sm-12 animate-in super-slow-mo" data-anim-type="fade-in-up" data-anim-delay="0">
+          <div class="our-service">
+            <div class="services row">
+              <div class="col-sm-4">
+                <div class="single-service">
+                  <i class="fa fa-th"></i>
+                  <h2>DESARROLLO DE SOFTWARE</h2>
+                  <p>EDS desarrolla aplicaciones web y de escritorio de acuerdo a necesidades.</p>
+                </div>
+              </div>
+              <div class="col-sm-4">
+                <div class="single-service">
+                  <i class="fa fa-desktop"></i>
+                  <h2>REDES</h2>
+                  <p>Diseño, instalación y configuración de redes, de acuerdo a tus necesidades.</p>
+                </div>
+              </div>
+              <div class="col-sm-4">
+                <div class="single-service">
+                  <i class="fa fa-laptop"></i>
+                  <h2>MANTENIMIENTO</h2>
+                  <p>Mantenimiento preventivo y correctivo a equipos de cómputo.</p>
+                </div>
+              </div></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section><!--/#service-->
+
+    <section id="clients">
+      <div class="container">
+        <div class="clients-wrapper">
+          <div class="row text-center">
+            <div class="col-sm-8 col-sm-offset-2 animate-in" data-anim-type="fade-in-up" data-anim-delay="0">
+              <h2 class="title-one">Clientes</h2>
+              <p> Desde el momento en que el cliente contacta a EDS el trato es profesional, nos dedicamos a escuchar a nuestros clientes para poder ofrecer las mejores  soluciones que mejor se adapten a sus necesidades laborales, ofreciendo una amplia gama de aplicaciones o herramientas que les permitan generar de manera eficiente  los procesos laborales, aprovechando al máximo la información que se genera en ellos, con la finalidad de mejorar los resultados.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section><!--/#clients-->
+
+            <section id="contact" class="parallax-section">
+              <div class="container">
+                <div class="row text-center clearfix">
+                  <div class="col-sm-8 col-sm-offset-2 animate-in" data-anim-type="fade-in-up" data-anim-delay="0">
+                    <div class="contact-heading">
+                      <h2 class="title-one">Contactanos</h2>
+                      <p class="single-service">EDS te ofrece los siguientes servicios, desarrollo de software, creación de redes y mantenimiento a equipo de cómputo para mantener tu negocio o empresa actualizada y a la vanguardia.</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="container">
+                <div class="contact-details">
+                  <!-- <div class="pattern"></div> -->
+                  <div class="row text-center clearfix animate-in super-slow-mo" data-anim-type="fade-in-up" data-anim-delay="0">
+                    <div class="col-sm-6">
+                      <div class="contact-address">
+                        <address class="single-service">
+                          <p><span>EDS</span> San Mateo Atenco</p><strong>Av. Juárez<br>Tel: (045) 722 555 5551<br> Tel: (045) 722 555 5551 <br> e-mail: eds_info@empresads.com </strong><br><strong>( Para mayor información, síguenos en nuestras redes sociales. )</strong>
+                        </address>
+                        <div class="social">
+                          <a href="#"><i class="fa fa-facebook"></i></a>
+                          <a href="#"><i class="fa fa-twitter"></i></a>
+                        </div>
+                      </div>
+                    </div>
+                    <!-- Start Contact -->
+                    <div class="col-sm-6"> 
+                      <div id="contact-form-section">
+                        <div class="status alert alert-success" style="display: none"></div>
+                          <form id="contact-form" onsubmit="return false;" class="contact" name="contact-form">
+                            <div class="form-group">
+                              <input type="text" name="name" class="form-control name-field" required="required" placeholder="Nombre">
+                            </div>
+                            <div class="form-group">
+                              <input type="email" name="email" class="form-control mail-field" required="required" placeholder="Email">
+                            </div> 
+                            <div class="form-group">
+                              <textarea name="message" id="message" required="required" class="form-control msj" rows="8" placeholder="Mensaje"></textarea>
+                            </div> 
+                            <div class="form-group">
+                              <button type="submit" class="btn btn-primary">Enviar</button>
+                            </div>
+                          </form> 
+                      </div>
+                    </div>
+                    </div>
+                  </div>
+                </div> 
+              </section> <!--/#contact--> 
+
+  <footer id="footer"> 
+    <div class="container"> 
+      <div class="text-center"> 
+        <p>Copyright &copy; 2016 - <a href="http://mostafiz.me/">Mostafiz</a> | All Rights Reserved</p> 
+      </div> 
+    </div> 
+  </footer> <!--/#footer--> 
+
+
+
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+	require 'app/controller/mvc.controller.php'; 
+	$mvc = new mvc_controller();	
+		$mvc->principal();
+?>
